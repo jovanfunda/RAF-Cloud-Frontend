9ae19744e3f5c1a716668292aaad65dcbd57c16c
Jovan Lazic <jovanfunda18@gmail.com>
2022-12-08 10:10:53 +0100

    Added updating users 	new file:   permission.service.ts 	modified:   show-users.service.ts 	modified:   ../home/home.component.html 	modified:   ../home/home.component.ts 	modified:   ../show-users/show-users.component.html 	modified:   ../show-users/show-users.component.ts 	modified:   ../update-user/update-user.component.html 	modified:   ../update-user/update-user.component.ts

--- src/app/service/permission.service.ts
+++ src/app/service/permission.service.ts
@@ -1,24 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { User } from '../classes/user';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ShowUsersService {
+export class PermissionService {
 
   private readonly apiUrl = environment.postApi;
 
   constructor(private httpClient: HttpClient) { }
 
-  getUsers(): Observable<any> {
-    return this.httpClient.post(`${this.apiUrl}/users`, localStorage.getItem("JWToken"));
+  checkPermission(_permission: string): Observable<any> {
+    return this.httpClient.post(`${this.apiUrl}/hasPermission`, {permission: _permission, jwtoken: localStorage.getItem("JWToken")});
   }
-
-  deleteUser(user: string): Observable<any> {
-    return this.httpClient.post(`${this.apiUrl}/deleteUser`, user);
-  }
-
 }
